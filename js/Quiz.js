76d3b6870664e31fd7d9c13fe2819744d3311f67
goyal-aryan <71602061+goyal-aryan@users.noreply.github.com>
2021-07-31 18:36:34 +0530

    issue fixed where text was not visible in the quiz

--- js/Quiz.js
+++ js/Quiz.js
@@ -16,6 +16,7 @@ class Quiz {
     }
 
     async start() {
+
         if (gameState === 0) {
             contestant = new Contestant();
             var contestantCountRef = await database.ref('contestantCount').once("value");
@@ -29,40 +30,36 @@ class Quiz {
     }
 
     play() {
-        //write code here to hide question elements
-        question.hide()
 
-        //write code to change the background color here
-        background("yellow")
-        fill(10);
-        textSize(35);
+        question.hide();
+        background("Yellow");
+        fill(0);
+        textSize(30);
+        text("Result of the Quiz are:", 340, 50);
+        text("----------------------------", 320, 65);
+        Contestant.getPlayerInfo();
 
-        //write code to show a heading for showing the result of Quiz
-        text("This is the result of the Quiz", 340, 50);
-
-        //call getContestantInfo( ) here
-        Contestant.getPlayerInfo()
-
-        //write condition to check if contestantInfor is not undefined
         if (allContestants !== undefined) {
+
+            debugger;
+            var display_Answers = 230;
             fill("Blue");
             textSize(20);
-            text("*Note:Contestant who answered correct are highlighted in green colour!", 130, 230)
+            text("*NOTE: Contestant who have answered correct are highlighted in green color!", 130, 230);
+
+            for (var plr in allContestants) {
+
+                debugger;
+                var correctAns = "2";
+                if (correctAns === allContestants[plr].answer)
+                    fill("Green")
+                else
+                    fill("red");
+
+                display_Answers += 30;
+                textSize(20);
+                text(allContestants[plr].name + ": " + allContestants[plr].answer, 250, display_Answers)
+            }
         }
-
-
-        for (var plr in allContestants) {
-            var correctAns = "2";
-            if (correctAns === allContestants[plr].answer)
-                fill("Green")
-            else
-                fill("red");
-
-            display_Answers += 30;
-            textSize(20);
-            text(allContestants[plr].name + ": " + allContestants[plr].answer, 250, display_Answers)
-        }
-
     }
-
 }
